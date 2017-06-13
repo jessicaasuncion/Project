bb6670520a01200f807ca3c57a7468673ec43efa
Skylord13 <Skyehager@gmail.com>
2017-06-13 15:10:45 -0700

    Add files via upload

---
+++ BookStore/BookStore/BookStore/BookInformationMenu.h
@@ -0,0 +1,15 @@
+#ifndef BOOKINFORMATIONMENU_H
+#define BOOKINFORMATIONMENU_H
+#include <string>
+using namespace std;
+
+class BookInformationMenu
+{
+private:
+	int numOptions;
+	string options[8];
+
+public:
+	
+};
+#endif 
