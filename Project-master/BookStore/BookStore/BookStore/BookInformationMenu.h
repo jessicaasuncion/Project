22e28ca4054df2d364f5eefe160dab307d8d3e43
Skylord13 <Skyehager@gmail.com>
2017-06-15 15:25:15 -0700

    Add files via upload

---
+++ Project-master/BookStore/BookStore/BookStore/BookInformationMenu.h
@@ -0,0 +1,27 @@
+#ifndef BOOKINFORMATIONMENU_H
+#define BOOKINFORMATIONMENU_H
+#include <string>
+#include <iomanip>
+#include "Books.h"
+#include "BookSearch.h"
+
+using namespace std;
+
+class BookInformationMenu
+{
+private:
+	int numOptions;
+	string options[8];
+
+public:
+	
+};
+#endif 
+
+void DisplayBookInformation(Books resultBooks[], int resultNumBooks)
+{
+	for (int i = 0; i < resultNumBooks; i += 0)
+	{
+		resultBooks[i].getI
+	}
+};
